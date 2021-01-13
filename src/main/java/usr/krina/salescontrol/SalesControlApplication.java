e50fb92639491b01b426f746c27a1f8c0fc3f9b0
spaubleit <spaubleit@gmail.com>
2021-01-13 22:49:13 +0300

    initial commit

---
+++ src/main/java/usr/krina/salescontrol/SalesControlApplication.java
@@ -0,0 +1,13 @@
+package usr.krina.salescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesControlApplication.class, args);
+	}
+
+}
